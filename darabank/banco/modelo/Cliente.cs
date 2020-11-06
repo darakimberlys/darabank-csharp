fd1fe1683f16bf42d5486fad8ab4b447517d3583
Dara Kimberly <darakimberlys@hotmail.com>
2020-11-05 23:40:13 -0300

    client class test

--- darabank/banco/modelo/Cliente.cs
+++ darabank/banco/modelo/Cliente.cs
@@ -2,8 +2,15 @@
 {
     internal class Cliente
     {
-        internal class getTitular
-        {
-        }
+        //verificar getters e setters
+        private string nome;
+        private string cpf;
+        private string profissao;
+
+        public string Nome { get; set; }
+    
+        public string Cpf { get; set; }
+
+        public string Profissao { get; set; }
     }
 }
