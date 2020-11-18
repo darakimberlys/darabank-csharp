a94d59f055fda3cb75b4f04ad2aeb60493ee14eb
Dara Kimberly <darakimberlys@hotmail.com>
2020-11-18 20:28:54 -0300

    new directory ro employees

---
+++ Darabank.Modelos/Funcionarios/Auxiliar.cs
@@ -0,0 +1,21 @@
+﻿namespace Darabank.Modelos.Funcionarios
+{
+    class Auxiliar : Funcionario
+    {
+
+        public Auxiliar(string cpf) : base(2000, cpf)
+        {
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.1;
+        }
+
+        internal protected override double GetBonificacao()
+        {
+            return Salario * 0.2;
+        }
+    }
+}
+
---
+++ Darabank.Modelos/Funcionarios/FuncionarioAutenticavel.cs
@@ -0,0 +1,18 @@
+﻿namespace Darabank.Modelos.Funcionarios
+
+{
+    public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
+    {
+        public string Senha { get; set; }
+
+        public FuncionarioAutenticavel(double salario, string cpf)
+            : base(salario, cpf)
+        {
+        }
+
+        public bool Autenticar(string senha)
+        {
+            return Senha == senha;
+        }
+    }
+}
